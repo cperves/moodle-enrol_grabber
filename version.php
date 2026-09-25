81b8999f70b656d83c0ab258468b9f1ce9003f3f
Celine Perves <cperves@unistra.fr>
2026-09-25 16:02:25 +0200

    need to increment for moodle.org

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026092501;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2026092502;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2024100701;        // Requires this Moodle version
 $plugin->component = 'enrol_grabber';    // Full name of the plugin (used for diagnostics)
-$plugin->release = '2.2.3';
+$plugin->release = '2.2.4';
 $plugin->maturity   = MATURITY_STABLE;
